3a7c57ca308fcd6d1ddbd5cb4b46a197c3e28a96
jiyasavaliya18 <126229948+jiyasavaliya18@users.noreply.github.com>
2023-04-29 01:50:09 +0530

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -64,7 +64,102 @@ public:
         Karnataka.name = "Karnataka : ";
         Karnataka.capital = "Bengaluru";
         cout<<Karnataka.name<<Karnataka.capital<<endl;
+        
+        IndianState Kerala;
+        Kerala.name = "Kerala : ";
+        Kerala.capital = "Thiruvananthapuram";
+        cout<<Kerala.name<<Kerala.capital<<endl;
+        
+        IndianState Madhya_Pradesh;
+        Madhya_Pradesh.name = "Madhya_Pradesh : ";
+        Madhya_Pradesh.capital = "Bhopal";
+        cout<<Madhya_Pradesh.name<<Madhya_Pradesh.capital<<endl;
+        
+        IndianState Maharashtra;
+        Maharashtra.name = "Maharashtra : ";
+        Maharashtra.capital = "mumbai";
+        cout<<Maharashtra.name<<Maharashtra.capital<<endl;
+            
+        IndianState Manipur;
+        Manipur.name = "Karnataka : ";
+        Manipur.capital = "Imphal";
+        cout<<Manipur.name<<Manipur.capital<<endl;
 
+        IndianState Meghalaya;
+        Meghalaya.name = "Meghalaya : ";
+        Meghalaya.capital = "Shillong";
+        cout<<Meghalaya.name<<Meghalaya.capital<<endl;
+    
+        IndianState Mizoram;
+        Mizoram.name = "Mizoram : ";
+        Mizoram.capital = "Mizoram";
+        cout<<Mizoram.name<<Mizoram.capital<<endl;
+    
+        IndianState karnataka;
+        Karnataka.name = "Karnataka : ";
+        Karnataka.capital = "Shillong";
+        cout<<Karnataka.name<<Karnataka.capital<<endl;
+    
+        IndianState Nagaland;
+        Nagaland.name = "Nagaland : ";
+        Nagaland.capital = "Aizawl";
+        cout<<Nagaland.name<<Nagaland.capital<<endl;
+    
+        IndianState Odisha;
+        Odisha.name = "Odisha : ";
+        Odisha.capital = "Bhubaneswar";
+        cout<<Odisha.name<<Odisha.capital<<endl;
+        
+        IndianState Punjab;
+        Punjab.name = "Punjab : ";
+        Punjab.capital = "Chandigarh";
+        cout<<Punjab.name<<Punjab.capital<<endl;
+    
+        IndianState odisha;
+        Odisha.name = "Odisha : ";
+        Odisha.capital = "Bhubaneswar";
+        cout<<Odisha.name<<Odisha.capital<<endl;
+    
+        IndianState Rajasthan;
+        Rajasthan.name = "Rajasthan : ";
+        Rajasthan.capital ="jaipur";
+        cout<<Rajasthan.name<<Rajasthan.capital<<endl;
+    
+        IndianState Sikkim;
+        Sikkim.name = "Sikkim : ";
+        Sikkim.capital = "Gangtok";
+        cout<<Sikkim.name<<Sikkim.capital<<endl;
+    
+        IndianState Tamil_Nadu;
+        Tamil_Nadu.name = "Tamil_Nadu : ";
+        Tamil_Nadu.capital = "Chennai";
+        cout<<Tamil_Nadu.name<<Tamil_Nadu.capital<<endl;
+    
+        IndianState Telangana;
+        Telangana.name = "Telangana : ";
+        Telangana.capital = "Hyderabad";
+        cout<<Telangana.name<<Telangana.capital<<endl;
+        
+        IndianState Tripura;
+        Tripura.name = "Tripura : ";
+        Tripura.capital = "Agartala";
+        cout<<Tripura.name<<Tripura.capital<<endl;
+        
+        IndianState Uttar_Pradesh;
+        Uttar_Pradesh.name = "Uttar_Pradesh : ";
+        Uttar_Pradesh.capital = "Lucknow";
+        cout<<Uttar_Pradesh.name<<Uttar_Pradesh.capital<<endl;
+        
+        IndianState Uttarakhand;
+        Uttarakhand.name = "Uttarakhand : ";
+        Uttarakhand.capital = "Dehradun (Winter),Gairsain (Summer)";
+        cout<<Uttarakhand.name<<Uttarakhand.capital<<endl;
+    
+        IndianState West_Bengal;
+        West_Bengal.name = "West_Bengal : ";
+        West_Bengal.capital = "Kolkata";
+        cout<<West_Bengal.name<<West_Bengal.capital<<endl;
+    
     }
 };
 
@@ -74,4 +169,3 @@ int main() {
     IndianState::printAllStates();
     return 0;
 }
-
