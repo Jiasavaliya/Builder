2641a8f15b733bd2b997c4da9d0524b2019c3e78
jiyasavaliya18 <126229948+jiyasavaliya18@users.noreply.github.com>
2023-05-04 12:45:00 +0530

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -3,33 +3,19 @@ using namespace std;
 
 class highschool{
 	
-	int stu_name,stu_roll_no,stu_standard, stu_age,stu_contact, stu_edu_institute_name,stu_address;
+	public:
 	
-};
-
-class college{
-public:
-
-    string stu_name, stu_roll_no,stu_standard, stu_age,stu_contact, stu_edu_institute_name,stu_address;
-    
-    
+	char stu_name[22];
+	int stu_roll_no;
+	int stu_standard;
+	int stu_age;
+	
    static int infor;
-   static void getter() {
-   college student;
-      
-	student.stu_name;
-        student.stu_roll_no;
-        student.stu_standard;
-        student.stu_age;
-        student.stu_contact;
-        student.stu_edu_institute_name;
-        student.stu_address;
-}
- 
-
+   
+   
     static void setter() {
         
-       college student;
+       highschool student;
       
       cout<<"student name : ";
       cin>> student.stu_name;
@@ -42,20 +28,70 @@ public:
 	  
       cout<<"student age : ";
       cin>> student.stu_age;
-	  
-      cout<<"student contact : ";
-      cin>> student.stu_contact;
-	  
-      cout<<"student contact : ";
-      cin>> student.stu_contact;
-	  
+	 
 }
- 
+   
+   static void getter() {
+   highschool student;
+      
+	    student.stu_name;
+        student.stu_roll_no;
+        student.stu_standard;
+        student.stu_age;
+        
+}
+
 };
 
-int college ::infor = 2; 
+class collage{
+	
+	public:
+	
+
+	char stu_contact[44];
+	char stu_edu_institute_name[55];
+	char stu_address[55];
+	
+	 
+   static int infor;
+   
+   
+    static void setter() {
+        
+       collage student;
+      
+     
+      cout<<"student contact : ";
+      cin>> student.stu_contact;
+	  
+      cout<<"student edu institute name : ";
+      cin>> student.stu_edu_institute_name;
+      
+      cout<<"student stu address : ";
+      cin>> student.stu_address;
+	
+}
+   
+   static void getter() {
+   collage student;
+      
+	  
+        student.stu_contact;
+        student.stu_edu_institute_name;
+        student.stu_address;
+}
+
+};
+
+
+
+
+int highschool ::infor = 2; 
+int collage ::infor = 2; 
 
 int main() {
-    college::setter();
+	
+    highschool::setter();
+    collage::setter();
     return 0;
 }
