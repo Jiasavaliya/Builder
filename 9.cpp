83e07519081da242ac41f7c53da2d8573e63348c
jiyasavaliya18 <126229948+jiyasavaliya18@users.noreply.github.com>
2023-05-04 18:11:47 +0530

    Update 9.cpp

--- 9.cpp
+++ 9.cpp
@@ -5,9 +5,11 @@ class RRS{
 	
 	private:
 		
-		int t_n;
-		char t_name[20];
-	
+	    int t_n;
+	    char t_name[20];
+	    char source[12];
+	    char destination[12];
+	    int t_time;
 	
 	public:
 		
@@ -17,6 +19,14 @@ class RRS{
 			cin>>t_n;
 			cout<<"enter train name : ";
 			cin>>t_name;
+			cout<<"enter train source : ";
+			cin>>source;
+			cout<<"enter train destination : ";
+			cin>>destination;
+		        cout<<"enter train train time : ";
+			cin>>t_time;
+		
+		
 		
 		}
 		
@@ -24,6 +34,10 @@ class RRS{
 			
 			cout<<"train number : "<<t_n<<endl;
 			cout<<"train name : "<<t_name<<endl;
+			cout<<"train source : "<<source<<endl;
+			cout<<"train destination : "<<destination<<endl;
+		        cout<<"train train time : "<<t_time<<endl;
+		
 		}
 		
 		int cheak_Trainnumber(){
@@ -56,17 +70,5 @@ int main(){
    		obj[i].get();
 	   }
    }
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
 
 }
